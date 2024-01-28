7a39a1be74ce5765e768222614e386eb9576a40f
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-01-28 10:52:38 -0800

    Added L3

---
+++ L3 (Time Complexity)/1-FrogJmp.py
@@ -0,0 +1,11 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+import math
+
+def solution(X, Y, D):
+    # Implement your solution here
+    delta = Y - X
+
+    jumps = math.ceil(delta/D)
+    return jumps
---
+++ L3 (Time Complexity)/3-TapeEquilibrium.py
@@ -0,0 +1,15 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A: list):
+    # Implement your solution here
+    left = A[0]
+    right = sum(A[1:])
+    min_diff = abs(left - right)
+    for i in range(1, len(A)-1):
+        left += A[i]
+        right -= A[i]
+        diff = abs(left - right)
+        if diff < min_diff:
+            min_diff = diff
+    return min_diff
